5be2ba99237127e59c2c2c32ab0db5567cfc8143
tanaymittal25 <tanay.mittal25@gmail.com>
2020-05-23 23:20:38 +0530

    mine_block

--- Create a Blockchain/blockchain.py
+++ Create a Blockchain/blockchain.py
@@ -63,3 +63,26 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
+
+#Mining Blockchain
+
+#Create Web App
+app = Flask(__name__)
+
+#Create
+blockchain = Blockchain()
+
+#Mine
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Block Created',
+    'index': block['index'],
+    'timestamp': block['timestamp'],
+    'proof': block['proof'],
+    'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
